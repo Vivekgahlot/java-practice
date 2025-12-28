fc83b1db0c42df7f52077925694b68047483a2ef
vivek gahlot <94837657+Vivekgahlot@users.noreply.github.com>
2025-12-28 22:13:07 +0530

    Add array element counting program
    Implement a program to count total elements in an array.

---
+++ java_array/ques2.java
@@ -0,0 +1,15 @@
+// Q:- Total element present in the array.
+
+package ArrayQustion;
+public class Ques2 {
+		public static void main(String[] args) {		
+		int count=0;
+		int arr[]= {3,7,8,2,5,6,4};		
+		for(int i=0;i<arr.length;i++) {
+			count++;
+		}
+		System.out.println(count);
+		
+	}
+
+}
