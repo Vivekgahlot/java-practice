b30c21db25f0206eb516fabb7912b52d2aa5b029
vivek gahlot <94837657+Vivekgahlot@users.noreply.github.com>
2025-12-28 22:14:45 +0530

    Add program to display last element of array
    Display the last element of an integer array.

---
+++ java_array/ques3.java
@@ -0,0 +1,12 @@
+ Display last element of the array.
+
+package Vivekgahlot;
+public class Ques5 {	
+	public static void main(String[] args) {
+	
+		int [] arr = {3,7,8,2,5,6,4};
+		System.out.println(arr [arr.length-1]);
+		
+	}
+
+}
