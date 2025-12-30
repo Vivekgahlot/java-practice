0b07555fb88aad3ffeea1b5e609fb71248b92b19
vivek gahlot <94837657+Vivekgahlot@users.noreply.github.com>
2025-12-30 22:34:09 +0530

    Add check for even first element in array
    Check if the first element of the array is even.

---
+++ java_array/ques5.java
@@ -0,0 +1,14 @@
+:- Check the first element of array is even or not.
+package vivekgahlot;
+public class Ques5 {
+	public static void main(String[] args) {
+		int [] arr = {3,7,8,2,5,6,4};
+		if(arr[0]%2==0) {
+			System.out.println("Even");
+		}
+		else {
+			System.out.println("Not");
+		}
+	}
+
+}
