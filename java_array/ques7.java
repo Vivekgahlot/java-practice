587d7ebf6e6f2bdf3c54f1c4cd1ef2553fa6ab55
vivek gahlot <94837657+Vivekgahlot@users.noreply.github.com>
2026-01-01 23:10:47 +0530

    Create ques7.java

---
+++ java_array/ques7.java
@@ -0,0 +1,11 @@
+// Q:- Print multiplication table for last element in the array.
+package Vivekgahlot;
+public class Ques7 {
+	public static void main(String[] args) {		
+		int [] arr = {3,7,8,2,5,6,4};
+		for(int i=1;i<10;i++) {
+			System.out.println(arr[arr.length-1]+" * " + i + " = " + arr[arr.length-1]*i);
+		}
+	}
+
+}
