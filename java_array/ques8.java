59f8beb6b8ef74573c1f8a66ddfec8cbebf2a050
vivek gahlot <94837657+Vivekgahlot@users.noreply.github.com>
2026-01-04 22:25:04 +0530

    add via upload
    Calculates and displays the sum of the two middle elements in an even-length array.

---
+++ java_array/ques8.java
@@ -0,0 +1,11 @@
+// Q:- Display sum of 2 middle elements in the given even array of elements.
+package Vivekgahlot;
+public class Ques8 {
+	    public static void main(String[] args) {
+	        int[] arr = {3, 7, 8, 2, 5, 6, 4, 1};
+	        int mid1 = arr.length / 2 - 1; 
+	        int mid2 = arr.length / 2;    
+	        int sum = arr[mid1] + arr[mid2];
+	        System.out.println(arr[mid1] + " and " + arr[mid2]+"----->"+sum);
+	    }
+}
