b3f9e734a3868b09ab96892e43ee4f557ff4b08b
vivek gahlot <94837657+Vivekgahlot@users.noreply.github.com>
2026-01-04 22:27:46 +0530

    Add files via upload

---
+++ java_array/ques9.java
@@ -0,0 +1,11 @@
+// Q:-Display all elements in an array.
+package Vivekgahlot;
+public class Ques9 {	
+	public static void main(String[] args) {		
+		int[] arr = {3, 7, 8, 2, 5, 6, 4};	
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}		
+	}
+
+}
