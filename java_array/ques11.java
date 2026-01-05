9f4153751972a1c5bae5983a8615008bd3046e76
vivek gahlot <94837657+Vivekgahlot@users.noreply.github.com>
2026-01-05 22:29:45 +0530

    Add files via upload
    Implement a program to find the sum of array elements.

---
+++ java_array/ques11.java
@@ -0,0 +1,12 @@
+// Q:- Find the sum of Array elements.
+package Vivekgahlot;
+public class Ques11 {
+	 public static void main(String[] args) {
+	        int[] arr = {3, 7, 8, 2, 5, 6, 4};       
+	        int sum=0;
+	        for(int i=0;i<arr.length;i++) {
+	        	sum=sum+arr[i];
+	        }
+	        System.out.println(sum);
+	    }
+}
