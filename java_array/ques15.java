26413731dc131404aa455cbf71153249b9e2d16c
vivek gahlot <94837657+Vivekgahlot@users.noreply.github.com>
2026-01-07 22:02:53 +0530

    Add files via upload

---
+++ java_array/ques15.java
@@ -0,0 +1,10 @@
+// Q:- Display elements using for each loop.
+package Vivekgahlot;
+public class Ques15 {
+	public static void main(String[] args) {	
+		int[] arr = { 3, 7, 8, 2, 5, 6, 4 };	
+		for(int i :arr) {
+			System.out.print(i+" ");
+		}
+	}
+}
