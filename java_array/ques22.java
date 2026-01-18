aa751c7b33a00bd1c38d8c12a8bcf1907cbae68a
vivek gahlot <94837657+Vivekgahlot@users.noreply.github.com>
2026-01-18 23:27:23 +0530

    Add files via upload
    Implement a program to find and display the largest element in an array.

---
+++ java_array/ques22.java
@@ -0,0 +1,16 @@
+// Q:- Display largest element in the array
+package Vivekgahlot;
+public class Ques22 {	
+	public static void main(String[] args) {		
+		int arr[] = { 3, 7, 8, 2, 5, 6, 4 };	
+		int max=arr[0];
+		
+		for(int i=0;i<arr.length;i++) {
+			if(max<arr[i]) {
+				max=arr[i];
+			}
+		}
+		System.out.println(max);
+	}
+
+}
