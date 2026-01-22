e4e86941e503d95d5394b356a611686b6c97a05d
vivek gahlot <94837657+Vivekgahlot@users.noreply.github.com>
2026-01-22 21:51:21 +0530

    Add files via upload

--- java_array/ques23.java
+++ java_array/ques23.java
@@ -1,6 +1,6 @@
 // Q:- Display smallest element in the array.
-package ArrayQustion;
-public class Ques26 {
+package Vivekgahlot;
+public class Ques23 {
 	public static void main(String[] args) {
 		int arr[] = { 3, 7, 8, 2, 5, 6, 4 };
 		int min = arr[0];
