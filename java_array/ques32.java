d914aa799023796d8edbac5dddc81c405d4ae1d8
vivek gahlot <94837657+Vivekgahlot@users.noreply.github.com>
2026-02-15 23:44:15 +0530

    Add files via upload
    Fixed the loop increment in the array copy operation.

--- java_array/ques32.java
+++ java_array/ques32.java
@@ -4,7 +4,6 @@ public class Ques32 {
 	public static void main(String[] args) {
         int arr1[] = {3, 7, 8, 2, 5, 6, 4};
         int arr2[] = new int[arr1.length];
-     
         for (int i = 0; i < arr1.length; i++) {
             arr2[i] = arr1[i];
         }
